77686d00522fc060b15e7d6211d7eee13f905749
june <jpark@spiria.com>
2020-10-20 16:56:25 -0400

    Added test for getting single profile

--- connect-scientist/spec/apis/profile_spec.rb
+++ connect-scientist/spec/apis/profile_spec.rb
@@ -12,12 +12,29 @@ describe "get all profile route", :type => :request do
     expect(JSON.parse(response.body)[0].keys).to eq(["id", "name", "short_website_url", "num_friends"])
   end
 
+  it 'returns correct name' do
+    expect(JSON.parse(response.body)[0]["name"]).to eq("june")
+  end
+
   it 'returns status code 200' do
     expect(response).to have_http_status(:success)
   end
+end
 
+
+describe "get one profile route", :type => :request do
+  let!(:profile) {create(:profile, name: "june", user: build(:user))}
+  before {get "/api/v1/profiles/#{profile.id}"}
+
+  it 'returns correct serialized values' do
+    expect(JSON.parse(response.body).keys).to eq(["id", "name", "long_website_url", "short_website_url", "payload", "friends"])
+  end
 
   it 'returns correct name' do
-    expect(JSON.parse(response.body)[0]["name"]).to eq("june")
+    expect(JSON.parse(response.body)["name"]).to eq("june")
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
   end
 end
